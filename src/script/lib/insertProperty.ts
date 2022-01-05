979387f7da9776e393a8b9f5fef8ec4a78ebec70
ochi <ocos54ce@gmail.com>
2022-01-05 22:10:39 +0900

    feat: set limit

--- src/script/lib/insertProperty.ts
+++ src/script/lib/insertProperty.ts
@@ -1,5 +1,5 @@
 import type { CardInfo, PropertyAddedTodo, Todo } from '../../@type';
-import { CANVAS_HEIGHT, CANVAS_WIDTH, CARD_SIZE } from '../setting';
+import { CANVAS_HEIGHT, CANVAS_WIDTH } from '../setting';
 
 function insertLevel(todos: Todo[]) {
 	todos.sort((a, b) => {
@@ -45,11 +45,7 @@ function insertLocation(todos: Todo[]) {
 		const separation = Math.floor(CANVAS_WIDTH / diff);
 
 		const result = (todo.importance - MIN_IMPORTANCE) * separation;
-		if (result + CARD_SIZE >= CANVAS_WIDTH) {
-			return CANVAS_WIDTH - 70;
-		} else {
-			return result;
-		}
+		return result;
 	}
 
 	function calculateTop(todo: Todo) {
@@ -57,12 +53,7 @@ function insertLocation(todos: Todo[]) {
 		const separation = Math.floor(CANVAS_HEIGHT / diff) === 0 ? 25 : Math.floor(CANVAS_HEIGHT / diff);
 
 		const result = ((new Date(todo.limit).getTime() - NEAREST_DATE) / 10000000) * separation;
-
-		if (result >= CANVAS_HEIGHT) {
-			return CANVAS_HEIGHT;
-		} else {
-			return result;
-		}
+		return result;
 	}
 }
 
--- src/script/lib/locate.ts
+++ src/script/lib/locate.ts
@@ -1,9 +1,20 @@
+import { CANVAS_HEIGHT, CANVAS_WIDTH, CARD_SIZE } from '../setting';
+
 function locate(todoLength: number) {
 	const todoCards = document.querySelectorAll<HTMLElement>('[data-id]');
 	for (let i = 0; i < todoLength; i++) {
 		const card = todoCards[i];
-		card.style.top = (card.dataset.top || '0') + 'px';
-		card.style.left = (card.dataset.left || '0') + 'px';
+		if (Number(card.dataset.left) + CARD_SIZE >= CANVAS_WIDTH) {
+			card.style.right = '0px';
+		} else {
+			card.style.left = (card.dataset.left || '0') + 'px';
+		}
+
+		if (Number(card.dataset.top) >= CANVAS_HEIGHT) {
+			card.style.bottom = '0px';
+		} else {
+			card.style.top = (card.dataset.top || '0') + 'px';
+		}
 	}
 }
 
