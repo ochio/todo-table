de12f2ac8419875c2e001420e0c5f0f7ffb617fc
ochi <ocos54ce@gmail.com>
2022-01-06 23:25:17 +0900

    feat: update position

--- src/script/lib/drag.ts
+++ src/script/lib/drag.ts
@@ -1,3 +1,5 @@
+import { OriginalTodo } from '../../@type';
+
 function drag(todoLength: number) {
 	const todoCards = document.querySelectorAll<HTMLElement>('[data-id]');
 	for (let i = 0; i < todoLength; i++) {
@@ -10,6 +12,19 @@ function draggable(target: HTMLElement) {
 
 	target.addEventListener('mousedown', mdown, false);
 	target.addEventListener('touchstart', mdown, false);
+
+	const todos: OriginalTodo[] = JSON.parse(localStorage.getItem('todos') || '[]');
+
+	const storageIndex = getIndex(target.dataset.id);
+
+	function getIndex(searchId?: string) {
+		for (let i = 0; i < todos.length; i++) {
+			if (todos[i].id === searchId) {
+				return i;
+			}
+		}
+		return -1;
+	}
 
 	function mdown(e: MouseEvent | TouchEvent) {
 		const event = e.type === 'mousedown' ? (e as MouseEvent) : (e as TouchEvent).changedTouches[0];
@@ -27,28 +42,50 @@ function draggable(target: HTMLElement) {
 
 		e.preventDefault();
 
-		target.style.top = event.pageY - y + 'px';
-		target.style.left = event.pageX - x + 'px';
+		setPosition('top', event.pageY - y);
+		setPosition('left', event.pageX - x);
 
-		if (Number(target.style.top.replace('px', '')) < 0) {
-			target.style.top = '0px';
-			mup();
-		}
-
-		if (Number(target.style.bottom.replace('px', '')) === 0) {
-			target.style.bottom = 'unset';
-		}
 		target.addEventListener('mouseup', mup, false);
 		document.body.addEventListener('mouseleave', mup, false);
 		target.addEventListener('touchend', mup, false);
 		document.body.addEventListener('touchleave', mup, false);
+
+		function setPosition(type: 'top' | 'right' | 'bottom' | 'left', position: number) {
+			let prop = String(position);
+			if (position < 0) {
+				prop = '0';
+				mup();
+			}
+
+			if (Number(target.style.bottom.replace('px', '')) === 0) {
+				target.style.bottom = 'unset';
+			}
+			if (Number(target.style.right.replace('px', '')) === 0) {
+				target.style.right = 'unset';
+			}
+
+			target.dataset[type] = prop;
+			target.style[type] = prop + 'px';
+		}
 	}
 
 	function mup() {
+		update();
 		document.body.removeEventListener('mousemove', mmove, false);
 		target.removeEventListener('mouseup', mup, false);
 		document.body.removeEventListener('touchmove', mmove, false);
 		target.removeEventListener('touchend', mup, false);
+
+		function update() {
+			if (storageIndex !== -1) {
+				todos[storageIndex].top = target.dataset.top != null ? target.dataset.top : '';
+				todos[storageIndex].left = target.dataset.top != null ? target.dataset.top : '';
+			} else {
+				throw new Error('invalid id');
+			}
+
+			localStorage.setItem('todos', JSON.stringify(todos));
+		}
 	}
 }
 
