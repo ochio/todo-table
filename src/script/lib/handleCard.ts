d19052bcaea30dc48da50461b68b3589f43651d5
ochi <ocos54ce@gmail.com>
2022-01-08 17:18:24 +0900

    feat: handleMenu

---
+++ src/script/lib/handleCard.ts
@@ -0,0 +1,4 @@
+const handleCard = {
+	update() {},
+	delete() {},
+};
---
+++ src/script/lib/handleMenu.ts
@@ -0,0 +1,10 @@
+function handleMenu() {
+	const menu = document.getElementById('menu');
+	if (menu == null) throw new Error('no menu');
+	const cards = Array.from(document.getElementsByClassName('js-card'));
+
+	for (let i = 0; i < cards.length; i++) {
+		const element = cards[i];
+		element.addEventListener;
+	}
+}
